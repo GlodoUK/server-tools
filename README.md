1c0540b72f057dedc09cccc1abc62e566f922029
Karl Southern <karl@theangryangel.co.uk>
2020-06-18 13:00:18 +0100

    Add mailgun module

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-# Web addons for Odoo
+# Server addons for Odoo
 
-This project aims to deal with modules related to the webclient of Odoo.
+This project adds various system level modules for Odoo. These may include things such as;
+
+- monitoring
+- mail
+- common helpers
---
+++ mailgun/controllers/main.py
@@ -0,0 +1,37 @@
+from odoo import http
+from odoo.http import request
+import hashlib, hmac
+
+class MailMailgun(http.Controller):
+
+    @http.route('/mailgun/notify', auth='public', type='http', csrf=False)
+    def mailgun_notify(self, **kw):
+        if not self._mailgun_verify(kw.get('token'), kw.get('timestamp'),
+                kw.get('signature')):
+            response = http.request.make_response("Invalid signature")
+            response.status_code = 406 # prevent mailgun from retrying.
+            return response
+
+        # mailgun notification in json format
+        message_url = kw.get('message-url')
+
+        request.env['mail.thread'].sudo().mailgun_fetch_message(message_url)
+        return 'Ok'
+
+    def _mailgun_verify(self, token, timestamp, signature):
+        if not signature:
+            return False
+
+        api_key = request.env['ir.config_parameter'].search([
+            ('key', '=', 'mailgun.apikey')
+        ], limit=1)
+
+        if not api_key or not api_key.value:
+            return False
+
+        hmac_digest = hmac.new(
+            key=bytes(api_key.value, "utf-8"),
+            msg='{}{}'.format(timestamp, token).encode("utf-8"),
+            digestmod=hashlib.sha256).hexdigest()
+
+        return hmac.compare_digest(signature, hmac_digest)
