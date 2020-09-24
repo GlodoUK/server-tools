1ad575fcb600a3277e29e273ece28f2d43292636
Karl Southern <karl@theangryangel.co.uk>
2020-09-24 15:20:59 +0100

    Fixes #8

--- mailgun/models/mail_thread.py
+++ mailgun/models/mail_thread.py
@@ -1,7 +1,5 @@
 import requests
-
 from odoo import models, api
-
 
 import logging
 _logger = logging.getLogger(__name__)
@@ -14,4 +12,18 @@ class MailThread(models.AbstractModel):
     def mailgun_fetch_message(self, message_url):
         api_key = self.env['ir.config_parameter'].sudo().get_param('mailgun.apikey')
         res = requests.get(message_url, headers={'Accept': 'message/rfc2822'}, auth=('api', api_key), verify=False)
-        self.message_process(False, res.json().get('body-mime'))
+
+        res_dict = res.json()
+        body_mime = res_dict.get('body-mime', '')
+        self.with_context(mailgun_override_recipients=res_dict.get('recipients')).message_process(False, body_mime)
+
+    @api.model
+    def message_parse(self, message, save_original=False):
+        res = super(MailThread, self).message_parse(message, save_original)
+
+        mailgun_override_recipients = self.env.context.get('mailgun_override_recipients', '')
+        if mailgun_override_recipients:
+            res['to'] = mailgun_override_recipients
+            res['recipients'] = mailgun_override_recipients
+
+        return res
