34275e926e9b075c65d80159c11dd991a430a1fa
Karl Southern <karl@theangryangel.co.uk>
2021-06-15 09:44:58 +0100

    Prevent X-Auto-Response-Suppress labelled emails from generating bounces

---
+++ sendgrid/models/mail_thread.py
@@ -0,0 +1,18 @@
+from odoo import models
+
+
+class MailThread(models.Model):
+    _inherit = "mail.thread"
+
+    def _routing_create_bounce_email(self, email_from, body_html, message, **mail_values):
+        # Stop ping-ponging on auto response systems.
+        # TODO: Submit this upstream.
+        if message.get("X-Auto-Response-Suppress", "") == "All":
+            return
+
+        return super()._routing_create_bounce_email(
+            email_from,
+            body_html,
+            message,
+            **mail_values
+        )
