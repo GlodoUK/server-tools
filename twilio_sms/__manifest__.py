7369608c1da08b6ef1c1682b2557da1b10750911
Karl Southern <karl@theangryangel.co.uk>
2023-04-18 14:05:11 +0100

    feat: initial twilio_sms 16.0 version

---
+++ twilio_sms/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "version": "16.0.1.0.0",
+    "name": "twilio_sms",
+    "summary": "Twilio SMS Gateway",
+    "author": "Glo Networks",
+    "website": "https://github.com/GlodoUK/server-tools",
+    "license": "Other proprietary",
+    "depends": ["sms"],
+    "external_dependencies": {"python": ["twilio", "phonenumbers"]},
+    "data": ["views/res_config_settings.xml"],
+}
---
+++ twilio_sms/models/res_config_settings.py
@@ -0,0 +1,21 @@
+from odoo import fields, models
+
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = "res.config.settings"
+
+    twilio_enabled = fields.Boolean(
+        config_parameter="twilio_sms.enabled",
+    )
+    twilio_sid = fields.Char(
+        config_parameter="twilio_sms.sid",
+    )
+    twilio_token = fields.Char(
+        config_parameter="twilio_sms.token",
+    )
+    twilio_from = fields.Char(
+        config_parameter="twilio_sms.from",
+    )
+    twilio_country_code = fields.Char(
+        config_parameter="twilio_sms.default_country_code",
+    )
